7a1369185ee7d81573787084a3a35d325f669e40
discoshrimp <roydega@gmail.com>
2018-07-12 20:32:58 -0700

    mvp final

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,47 +1,50 @@
 var searchTerm = "";
+
+var omdbResponse = {
+	"title": [],
+	"year": [],
+	"rated": [],
+	"released": [],
+	"runtime": [],
+	"actors": [],
+	"awards": [],
+	"boxOffice": [],
+	"country": [],
+	"director": [],
+	"genre": [],
+	"language": [],
+	"metascore": [],
+	"plot": [],
+	"poster": [],
+	"production": [],
+	"writer": [],
+	"imdbId:": [],
+	"imdbRating": [],
+	"imdbVotes": [],
+	"metacritic": [],
+	"rottom": [],
+}
+
+var movieDBdata = {
+	"voteAvg": 0,
+	"voters": 0,
+	"revenue": 0,
+	"budget": 0,
+}
 $(document).ready(function () {
 	function clearForm() {
 		$("#searchBox").val("")
 	}
 	$("#searchButton").on("click", function (event) {
+
+		//-----------API CALLS-------------//	
 		searchTerm = $("#searchBox").val().trim();
 
 		var omdbData = "http://www.omdbapi.com/?t=" + searchTerm + "&apikey=3efbbefc";
-		var omdbResponse = {
-			"title": [],
-			"year": [],
-			"rated": [],
-			"released": [],
-			"runtime": [],
-			"actors": [],
-			"awards": [],
-			"boxOffice": [],
-			"country": [],
-			"director": [],
-			"genre": [],
-			"language": [],
-			"metascore": [],
-			"plot": [],
-			"poster": [],
-			"production": [],
-			"writer": [],
-			"imdbId:": [],
-			"imdbRating": [],
-			"imdbVotes": [],
-			"metacritic": [],
-			"rottom": [],
-		}
 
-		var movieDBdata = {
-			"voteAvg": 0,
-			"voters": 0,
-			"revenue": 0,
-			"budget": 0,
-		}
 		event.preventDefault();
 		console.log("click success!");
 		console.log(searchTerm);
-
 
 		$.ajax({
 			url: omdbData,
@@ -71,9 +74,7 @@ $(document).ready(function () {
 			omdbResponse.imdbVotes = omdbDataResponse.imdbVotes;
 			omdbResponse.metacritic = omdbDataResponse.Ratings[2].Value;
 			omdbResponse.rottom = omdbDataResponse.Ratings[1].Value;
-			var imdb = omdbResponse.imdbRating * 10;
-			var rt = parseInt(omdbResponse.rottom);
-			var meta = omdbResponse.metacritic;
+
 			//movieDB ajax
 			var movieDB = "https://api.themoviedb.org/3/movie/" + omdbResponse.imdbId + "?api_key=15f49e312d668cfea4632253c8087323&language=en-US";
 			$.ajax({
@@ -96,6 +97,43 @@ $(document).ready(function () {
 				$("#mcScore").html("MetaCritic: " + omdbResponse.metacritic);
 				console.log(movieDBdata);
 				console.log(omdbResponse);
+
+
+				//---------------Slider Code------------------------//	
+				var imdb = parseInt(omdbResponse.imdbRating*10);
+				var mdb = parseInt(movieDBdata.voteAvg*10);
+				var rt = parseInt(omdbResponse.rottom);
+				var meta = parseInt(omdbResponse.metacritic);
+				console.log(imdb);
+				console.log(mdb);
+				console.log(rt);
+				console.log(meta);
+				var criticScore = (rt * 0.4) + (meta * 0.6);
+				var userScore = (mdb * 0.65) + (imdb * 0.35);
+				console.log(criticScore);
+				console.log(userScore);
+
+				var minScore = Math.min(userScore, criticScore);
+
+				var midScore = Math.ceil(Math.abs(userScore - criticScore));
+
+				var eachNotch = (midScore / 499);
+
+				var initScore = Math.ceil(minScore + (midScore / 2));
+
+				var slider = document.getElementById("myRange");
+
+				var output = document.getElementById("rating");
+				//output.innerHTML = slider.value;
+				output.innerHTML = initScore;
+
+				slider.oninput = function () {
+					//output.innerHTML = this.value;
+					output.innerHTML = (minScore + ((this.value - 1) * eachNotch)).toFixed(1);
+				}
+
+				$("#Score").html("Critic Score: " + criticScore + "<br> Viewer Score: " + userScore);
+
 			});
 		});
 		clearForm()
